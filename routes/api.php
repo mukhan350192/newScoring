58402d70a3cd4301dad7a63d8d1b4b452873c524
mukhan350192 <mukhan350192@gmail.com>
2022-06-13 13:54:04 +0600

    update

--- routes/api.php
+++ routes/api.php
@@ -21,3 +21,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::get('/xdata',[xData::class,'index']);
 Route::get('/test',[xData::class,'create']);
 Route::get('/susn',[xData::class,'susn']);
+Route::get('/testQueue',[xData::class,'testQueue']);
